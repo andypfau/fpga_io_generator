ce3b03d136fe0d812da772238d0cb76e251d3c46
Andreas Pfau <andreas.pfau@apfau.de>
2022-11-29 18:06:37 +0100

    Worked on FPGA demo

--- samples/vivado_cmoda7/software/test.py
+++ samples/vivado_cmoda7/software/test.py
@@ -1,7 +1,7 @@
 from buttons import Buttons
-from pwm_reg import PWM_Reg
-from sweep_reg import Sweep_Reg
-from leds import LEDs
+from pwm_gen import PwmGen
+from sweep_gen import SweepGen
+from leds import Leds
 from serialcomm import SerialComm
 import time
 
@@ -12,33 +12,30 @@ PORTNAME = 'COM11'
 
 com = SerialComm(PORTNAME, verbose=False)
 buttons = Buttons(com)
-sweep = Sweep_Reg(com)
-leds = LEDs(com)
-pwm = PWM_Reg(com)
+sweep = SweepGen(com)
+leds = Leds(com)
+pwm = PwmGen(com)
 
 
 # for i in range(9999999):
-#     leds.set_ctrl_led1_masked(True if i % 2 == 0 else False)
-#     leds.set_ctrl_led2_masked(True if i % 2 != 0 else False)
+#     leds.set_control_led_1_masked(True if i % 2 == 0 else False)
+#     leds.set_control_led_2_masked(True if i % 2 != 0 else False)
 #     print(f'Buttons: {buttons.get_buttons_btn1()}, {buttons.get_buttons_btn2()}')
 #     time.sleep(0.2)
 
-leds.set_ctrl_led1_masked(True)
-leds.set_ctrl_led2_masked(False)
+leds.set_control_led_1_masked(True)
+leds.set_control_led_2_masked(False)
 
 print(buttons.get_buttons_btn1())
 print(buttons.get_buttons_btn2())
 
-# sweep.set_ctrl1_en_shadow(False, flush=True)
+# sweep.set_control_1_en_shadow(False, flush=True)
 # pwm.set_red_value_overwrite(200)
 # pwm.set_green_value_overwrite(0)
 # pwm.set_blue_value_overwrite(0)
 
-sweep.set_ctrl1_delay_shadow(1500)
-sweep.set_ctrl2_incr_shadow(5)
-sweep.set_ctrl2_max_shadow(200)
-sweep.set_ctrl1_en_shadow(True)
+sweep.set_control_1_delay_shadow(1500)
+sweep.set_control_2_incr_shadow(5)
+sweep.set_control_2_max_shadow(200)
+sweep.set_control_1_en_shadow(True)
 sweep.flush_shadow()
-
-# com.write_reg(0x20, (1<<0) | (1000<<1))
-# com.write_reg(0x24, (2<<0) | (100<<16))
